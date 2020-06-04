ba074b31e12ee4a74b2e155475d3feaf2c43b3b9
Perla SC <perlascabrera@gmail.com>
2020-06-04 09:24:12 -0500

    Aplicando animaciones a React

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import logoPlatzi from './platzi.png'
+import CSSTransitionGroup from 'react-addons-css-transition-group'
 
 function App() {
+  state = {
+    logo:logo,
+  }
+  onClick() {
+this.setState({
+  logo: logoPlatzi
+})
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img key={this.state.logo} src={this.state.logo} className="App-logo" alt="logo" />
+        <CSSTransitionGroup transitionName='fade' transitionEnterTimeout={2000} transitionLeaveTimeout={1000} />
         <h1> Hello world ! </h1>{" "}
+        <buttom onClick={this.onClick.bind(this)}>click me!</buttom>
         <p>
           Edit <code> src / App.js </code> and save to reload.{" "}
         </p>{" "}
