5762da5869fa3155bd166449174f8656d28af117
Perla SC <perlascabrera@gmail.com>
2020-06-06 09:53:21 -0500

    haciendo que los datos de la página invie sean dinámicos

--- invie/src/index.js
+++ invie/src/index.js
@@ -1,18 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/invie.css';
-import App from './invie';
 import Portada from './components/Portada';
 import Guitarras from './components/Guitarras.jsx';
 import Footer from './components/Footer.jsx';
 import * as serviceWorker from './serviceWorker';
+import logoPortada from './images/invie.png'
+import acustica from './images/invie-acustica.png'
+import classic from './images/invie-classic.png'
 
+const data = {
+  menu: [{
+    href: 'index.html',
+    title: 'Home',
+  }, {
+    href: '#guitarras',
+    title: 'Guitarras'
+  }, {
+    href: 'precios.html',
+    title: 'Precios',
+  } ],
+  logoPortada: logoPortada,
+  guitarras: [{
+    image: acustica,
+    alt: 'Guitarra Invie Acustica',
+    name: 'Invie Acústica',
+    features: [
+      'Estilo Vintage',
+      'Madera pura',
+      'Incluye estuche invisible de aluminio'
+    ]
+  } ,
+  {
+    image: classic,
+    alt: 'Guitarra Invie Classic',
+    name: 'Invie Classic',
+    features: [
+      'Estilo Vintage',
+      'Liviana',
+      'empiezas tu canino como rockstar'
+    ]
+}]
+}
 ReactDOM.render(
   <React.StrictMode>
     <section className='invie'>
-      <Portada />
+      <Portada menu={data.menu} logo={data.logoPortada} />
       {/*Portada*/}
-      <Guitarras />
+      <Guitarras guitarras={data.guitarras} />
       {/*Guitarras*/}
       <Footer/>
       {/*footer*/}
